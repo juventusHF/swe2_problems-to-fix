79c3557c76bfa9c316c4e47e9277b86e06c622ed
gitLinda <lkrueger17@gmail.com>
2017-10-01 11:45:50 +0200

    remove all solution comments

--- src/ch/juventus/se/problemstofix/bird/Bird.java
+++ src/ch/juventus/se/problemstofix/bird/Bird.java
@@ -6,8 +6,6 @@ package ch.juventus.se.problemstofix.bird;
  */
 public abstract class Bird {
 
-    private int testline;
-
     private int age;
     private double weight;
     private boolean isHeathy;
